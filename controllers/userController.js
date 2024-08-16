a54c598c6a89e501cedc9750de16d3cc4d87de73
ingaSiu <ingasiud@gmail.com>
2024-08-16 14:06:48 +0300

    add all user controller functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,23 @@
-const authUser = (req, res) => {
-  res.status(200).json({ message: 'Auth user' });
-};
+import asyncHandler from 'express-async-handler';
 
-export { authUser };
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Auth user' });
+});
+
+const registerUser = asyncHandler(async, (req, res) => {
+  res.status(200).json({ message: 'RegisterUser' });
+});
+
+const logoutUser = asyncHandler(async, (req, res) => {
+  res.status(200).json({ message: 'Logout user' });
+});
+
+const getUserProfile = asyncHandler(async, (req, res) => {
+  res.status(200).json({ message: 'Get user profile' });
+});
+
+const updateUserProfile = asyncHandler(async, (req, res) => {
+  res.status(200).json({ message: 'Update user' });
+});
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
