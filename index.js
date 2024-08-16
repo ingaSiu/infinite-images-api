b4d430b58c20d2b93d90b1718c77c653a6132bd9
ingaSiu <ingasiud@gmail.com>
2024-08-16 12:49:40 +0300

    add userRoutes to server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import userRoutes from './routes/userRoutes.js';
 
-const port = 5000;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
