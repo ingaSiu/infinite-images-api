63fa24977100334b95f677908a0c334da5b3ec7b
ingaSiu <ingasiud@gmail.com>
2024-08-18 08:30:00 +0300

    update authUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,21 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js';
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth user' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
