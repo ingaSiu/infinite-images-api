6b1e9d75d42f1592bbe7032fb62828c569c20582
ingaSiu <ingasiud@gmail.com>
2024-08-18 08:38:22 +0300

    use cookie parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
@@ -12,6 +13,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
