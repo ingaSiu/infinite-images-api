c7256afb2b060e718e2688bc2e90ff76ecdb35fe
ingaSiu <ingasiud@gmail.com>
2024-08-18 13:44:54 +0300

    fix- findById spelling

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,14 +11,17 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.FindById(decoded.userId).select('-password');
+      req.user = await User.findById(decoded.userId).select('-password');
 
       next();
     } catch (error) {
+      // TODO:remove this
+      throw error;
       res.status(401);
       throw new Error('Not authorized, invalid token');
     }
   } else {
+    throw error;
     res.status(401);
     throw new Error('Not authorized, no token');
   }
