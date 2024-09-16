75e2fa4b2734bbe8c18fbe0112aa064f987003a1
ingaSiu <ingasiud@gmail.com>
2024-09-16 16:44:45 +0300

    add cors

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
